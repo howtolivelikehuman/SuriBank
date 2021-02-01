f48d4cc30a16426595299ee0c8f0171ba7c09409
howtolivelikehuman <yoicch19@gmail.com>
2021-02-01 11:22:47 +0900

    Merge into main

--- SuriBack/src/main/java/com/uos/suribank/dto/UserVO.java
+++ SuriBack/src/main/java/com/uos/suribank/dto/UserVO.java
@@ -2,6 +2,8 @@ package com.uos.suribank.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,10 +13,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserVO implements Serializable{
 	int _no;
+	
+	@Size(min=4, max=20, message = "id는 4~20자로 입력해야 합니다.")
 	String id;
+	//@Size(min=8, max=20, message = "password는 8~20자로 입력해야 합니다.")
 	String password;
+
+	@Size(min=2, max=8, message = "이름은 2~8자로 입력해야 합니다.")
 	String name;
+
+	@Size(min=1, max=20, message = "별명은 1~20자로 입력해야 합니다.")
 	String nickname;
+
+	@Size(max=30, message = "이메일은 30자 이하로 입력해야 합니다.")
 	String email;
 	String major;
 	//T1 is the lowest. T5 is admin
--- SuriBack/src/main/java/com/uos/suribank/exception/CustomizedResponseEntityExceptionHandler.java
+++ SuriBack/src/main/java/com/uos/suribank/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.uos.suribank.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,19 +14,29 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestController
 @ControllerAdvice
-public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Object> handleAllExceptions(Exception e, WebRequest request){
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), e.getMessage(), request.getDescription(false));
+    public final ResponseEntity<Object> handleAllExceptions(Exception e, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), e.getMessage(),
+                request.getDescription(false));
 
         return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public final ResponseEntity<Object> handleUserNotFoundExceptions(Exception e, WebRequest request){
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), e.getMessage(), request.getDescription(false));
+    public final ResponseEntity<Object> handleUserNotFoundExceptions(Exception e, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), e.getMessage(),
+                request.getDescription(false));
 
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Validation Error" ,ex.getBindingResult().toString()); 
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
 }
