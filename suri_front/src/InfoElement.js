d72c7295ab8ca578fc0d6b799f2e17382408b731
gyuZzang <tndud0606@gmail.com>
2021-02-01 14:41:35 +0900

    except pw, _no

--- suri_front/src/InfoElement.js
+++ suri_front/src/InfoElement.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 
 function InfoElement({k, v}){
-    return(
-        <div className="row info_element">
-            <div className="col-sm-7 key">
-                {k}
+    if (k=='_no'||k=='password') return ""
+    else{
+        return(
+            <div className="row info_element mb-3 ">
+                <div className="col-sm-7 key">
+                    {k}
+                </div>
+                <div className="col-sm-5 value">
+                    {v}
+                </div>
             </div>
-            <div className="col-sm-5 value">
-                {v}
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 
