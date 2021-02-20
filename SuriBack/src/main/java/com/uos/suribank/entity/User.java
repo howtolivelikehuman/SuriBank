90a03a64f148c9d4de7f96715edd6e154abea98f
howtolivelikehuman <yoicch19@gmail.com>
2021-02-21 00:53:04 +0900

    Clean up Code

--- SuriBack/src/main/java/com/uos/suribank/entity/User.java
+++ SuriBack/src/main/java/com/uos/suribank/entity/User.java
@@ -3,9 +3,7 @@ package com.uos.suribank.entity;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.persistence.Column;
