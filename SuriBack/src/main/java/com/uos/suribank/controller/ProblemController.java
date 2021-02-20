560882201faea3d32bbb929b3282c095d7717e9f
GyuZzang <tndud0606@gmail.com>
2021-02-21 01:03:25 +0900

    makePB(multi img) fot test (#22)
    * err: id to email
    
    * get problem content,
    modify logo
    
    * Problem: get pb by id
    Main: get pb list error!
    
    * get pb list (get -> post)
    
    * make PB (except img)
    
    * filter(html)
    
    * solve err make pb
    
    * makePB with img (for test)
    
    Co-authored-by: HowtoLiveLikehuman <63439738+howtolivelikehuman@users.noreply.github.com>

--- SuriBack/src/main/java/com/uos/suribank/controller/ProblemController.java
+++ SuriBack/src/main/java/com/uos/suribank/controller/ProblemController.java
@@ -2,6 +2,7 @@ package com.uos.suribank.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
