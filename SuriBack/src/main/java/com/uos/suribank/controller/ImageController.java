e3127cd77cf3c341209f7ea18c0e19cd296b9526
howtolivelikehuman <yoich_@naver.com>
2021-03-09 23:19:52 +0900

    no message

--- SuriBack/src/main/java/com/uos/suribank/controller/ImageController.java
+++ SuriBack/src/main/java/com/uos/suribank/controller/ImageController.java
@@ -8,7 +8,6 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/images")
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class ImageController {
      //이미지 return
      @GetMapping(value = "/{foldername}/{filename}", produces = MediaType.IMAGE_JPEG_VALUE)
--- SuriBack/src/main/java/com/uos/suribank/service/ProblemService.java
+++ SuriBack/src/main/java/com/uos/suribank/service/ProblemService.java
@@ -38,50 +38,6 @@ public class ProblemService {
 
     
     @Transactional
-    public boolean addProblem2(problemAddinfoDTO pAddinfoDTO, 
-    List<MultipartFile> q_img, List<MultipartFile> a_img) throws Exception{
-       
-        boolean result = problemRepository.addProblem(pAddinfoDTO);
-        Long problem_id = problemRepository.getProblemId(pAddinfoDTO.getTitle(), pAddinfoDTO.getProfessor());
-
-        String a_path[] = null;
-        String q_path[] = null;
-
-        if(q_img != null){
-            q_path = uploadImage(q_img, 'Q', problem_id);
-        }
-
-        if(a_img != null){
-            a_path = uploadImage(a_img, 'A', problem_id);
-        }
-        
-        problemRepository.addImages(q_path,a_path,problem_id);
-        return result;
-    }
-
-    public String[] uploadImage2(List<MultipartFile> files, char type, Long id) throws Exception {
-        String[] pathList = new String[files.size()];
-        String absolutepath = System.getProperty("user.dir")+"/src/main/resources";
-        String path = "/images/" + id;
-
-        File folder = new File(absolutepath+path);
-        if(!folder.exists()){
-            folder.mkdir();
-        }
-
-        for(int i=0; i< files.size(); i++){
-            String name = ""+id+type+i;
-
-            //absolute path in this project
-            String filename = path +"/"+name+".jpg";
-            pathList[i] = filename;
-            files.get(i).transferTo(new File(absolutepath + filename));
-        }
-        return pathList;
-    }
-
-    
-    @Transactional
     public boolean addProblem(problemAddinfoDTO pAddinfoDTO, 
     List<MultipartFile> q_img, List<MultipartFile> a_img) throws Exception{
        
@@ -105,7 +61,6 @@ public class ProblemService {
 
     public String[] uploadImage(List<MultipartFile> files, char type, Long id) throws Exception {
         String[] pathList = new String[files.size()];
-
         String absolutepath = System.getProperty("user.dir")+"/src/main/resources";
         String path = "/images/" + id;
 
@@ -119,7 +74,7 @@ public class ProblemService {
 
             //absolute path in this project
             String filename = path +"/"+name+".jpg";
-            pathList[i] = absolutepath+filename;
+            pathList[i] = filename;
             files.get(i).transferTo(new File(absolutepath + filename));
         }
         return pathList;
